9addae760e41f9e7129ece65ca66ab0963928cc7
Safee1282 <safeesajid1282@gmail.com>
2025-03-04 17:44:37 +0530

    initial commit

---
+++ variables.py
@@ -0,0 +1,32 @@
+name="safee"
+rollNo= 18
+print("Your name is " , name)
+num1=154
+num2=3
+print("the product of num1 and num2 is " ,(num1*num2) )
+num3=num4=123456789
+print(num3)
+print(num4)
+
+
+x,y,z=12345,3456,6789
+print("X: ", x)
+print("Y: ", y)
+print("Z: ", z)
+
+s=input("Enter your favourite game : ")
+print("your favourite game is " , s)
+n1=int(input("Enter your favourite number : " ))
+n2=int(input("Enter your friends favourite number : " ))
+product=n1*n2
+print("the product is " , product)
+
+import keyword
+print(keyword.kwlist)
+
+
+
+l=int(input("Enter length of rectangle : "))
+b=int(input("Enter breadth of rectangle : "))
+perimeter=2*(l+b)
+print("the perimeter of the rectangle is " , perimeter)
