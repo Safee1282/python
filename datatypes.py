6b6f2fd4daa10a664dd11bf9fce89e10cc56abbf
Safee1282 <safeesajid1282@gmail.com>
2025-03-06 17:45:25 +0530

    initial commit

---
+++ datatypes.py
@@ -0,0 +1,30 @@
+game="free fire"
+print(type(game))
+cricket=11
+print(type(cricket))
+win=False
+print(type(win))
+percentage=70.33
+print(type(percentage))
+f=int(percentage)
+print(f)
+r=float(cricket)
+print(r)
+s="hello sultan"
+print(len(s))
+str=""" this is sultan 
+he is a good boy
+he is my brother"""
+print(str)
+print(s[4])
+print(s[0:6])
+print(s[2:])
+print(s[::-2])
+print(s[-8:-2])
+s1="sultan"
+s2="-e-garama"
+print(s1+s2)
+s3="safee"
+s4="SAJID"
+print(s3.upper())
+print(s4.lower())
