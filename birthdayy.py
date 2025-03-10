39c0b5f12ad5f5e0fc2d10ff12df564e267df1de
Safee1282 <safeesajid1282@gmail.com>
2025-03-10 14:22:59 +0530

    initial commit

---
+++ birthdayy.py
@@ -0,0 +1,11 @@
+my_birthday="6 Feburary 2013"
+sister="5 September 2014"
+brother="21 June 2019"
+mom="8 August 1978"
+dad="3 April 1978"
+print("I am born on " , my_birthday)
+print("My sister is born on " , sister)
+print("My brother is born on " , brother)
+print("My mom is born on " , mom)
+print("My dad is born on " , dad)
+
