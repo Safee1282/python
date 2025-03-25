c977e5fb67ab61ab514ed3244bda51526686231e
Safee1282 <safeesajid1282@gmail.com>
2025-03-25 17:43:33 +0530

    initial commit

---
+++ sum.py
@@ -0,0 +1,31 @@
+# Write a program to calculate the sum of whole numbers.
+
+# sum=0
+# n=int(input("Enter a number : "))
+# for i in range(0,n+1):
+#     sum+=i
+# print("the sum is : ", sum)
+
+# Write a program to reverse the string entered by the user.
+
+# s1=input("Enter a word : ")
+# s2=("")
+# for i in s1 :
+#     s2=i+s2
+# print("Original string : ",s1)    
+# print("Reversed string : ", s2)
+
+# Write a program to print the numbers in reverse order beginning from the number entered by the user.
+
+# n=int(input("Enter a number : "))
+# for i in range(n,0,-1):
+#   print(i)    
+    
+
+# for i in range(1,51,2):
+#     print(i)
+
+n=int(input("Enter a number : "))
+for i in range(1,11):
+     print(n,"x",i,"=",n*i)
+
