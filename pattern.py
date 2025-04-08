78852503d53fc6cb4ff2ae86504125b1da0e120f
Safee1282 <safeesajid1282@gmail.com>
2025-04-08 17:46:24 +0530

    initial commit

---
+++ pattern.py
@@ -0,0 +1,40 @@
+
+#Right Angled Triangle pattern
+# for i in range(10):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()    
+#floyds triangle
+# num=1
+# for i in range(5):
+#      for j in range(i+1):
+#          print(num , end=" ")
+#          num=num+1
+#      print()    
+
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print(i,end=" ")
+#     print()    
+
+#diamond pattern
+
+
+for i in range(5,0, -1):
+    for j in range(0,i):
+        print("#",end=" ")
+    print(" ")
+
+
+
+
+
+
+
