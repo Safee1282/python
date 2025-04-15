ef01287a1271e8492673addbd21373bd2e0036c4
Safee1282 <safeesajid1282@gmail.com>
2025-04-15 17:49:43 +0530

    initial commit

---
+++ function.py
@@ -0,0 +1,40 @@
+# def demo(name):
+#     print("hello",name)
+#     print("I am your new teacher")
+
+# demo("safee")
+
+# def sum(a,b):
+#     c=a+b 
+#     print("The sum is ",c)
+# n=int(input("Enter a random number : "))    
+# n1=int(input("Enter another random number : "))    
+# sum(n,n1)
+
+
+def add(a,b):
+    return a+b 
+def sub(a,b):
+    return a-b
+def into(a,b):
+    return a*b
+def by(a,b):
+    return a/b
+print("a,Addition")
+print("b,subtraction")
+print("c,multiplication")
+print("d,division")
+ask=str(input("Choose an operation "))
+n=int(input("Enter a  number : "))    
+n1=int(input("Enter another number : ")) 
+if ask=="a":
+    print("the sum of numbers entered is ",add(n1,n))
+elif ask=="b":
+    print("the difference of numbers entered is ",sub(n,n1))
+elif ask=="c":
+    print("the product of numbers entered is ",into(n,n1))  
+else :
+    print("the quotient of numbers entered is ",by(n,n1))      
+    
+
+    
---
+++ turtle23.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.Screen().bgcolor("cyan")
+turtle.Screen().setup(400,500)
+t=turtle.Turtle()
+for i in range(4):
+  t.fd(100)
+  t.right(90)
+turtle.done()
