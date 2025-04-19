e1b5d8529bd1a5228e6d5b876a315cb4ae17e240
Safee1282 <safeesajid1282@gmail.com>
2025-04-19 21:36:07 +0530

    initial commit

---
+++ circle.py
@@ -0,0 +1,8 @@
+def circle(r,s=6.28):
+    circumference=r*s
+    print("The circumference of a circle with the given radius is ",circumference)
+
+radius=float(input("Enter the radius : "))
+circle(radius)
+
+
---
+++ shutdown.py
@@ -0,0 +1,12 @@
+print(" yes")
+print(" no")
+print("(put your answer in lower case)")
+shutdown = str(input("Do You want to shutdown your device "))
+if shutdown =="yes":
+    print("Shutting down the device")
+elif shutdown =="no":
+    print("Shutdown aborted")
+else :
+    print("Sorry")    
+
+
