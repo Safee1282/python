91eaf90a57bc487a72050ca91a7fbad14e426743
Safee1282 <safeesajid1282@gmail.com>
2025-05-27 17:43:14 +0530

    initial commit

---
+++ set.py
@@ -0,0 +1,19 @@
+# Write a program to create a set and perform the following operations on that set- 1. Create a set with integer elements 2. Create a set with mixed data type elements 3. Create another set with elements - 1, 2, 3, 4, 3, 2 4. Create a set from a list with elements - [1, 2, 3, 2] 5. Print the set after removing the first element from this set - [0, 1, 3, 4, 5]
+integer={1,-2,46,98,5,-55}
+mix={"safee",1,34}
+element={1,2,3,4,3,2,4}
+my=set([1,2,3,2])
+first={0,1,3,4,5}
+print(first)
+first.pop()
+print(first)
+print(my)
+print(element)
+print(mix)
+print(integer)
+set1={"green","blue","red","purple"}
+set2={"magenta","violet","red","blue"}
+set3=set1.intersection(set2)
+print(set1)
+print(set2)
+print(set3)
